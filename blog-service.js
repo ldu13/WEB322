594e5ecfda617350ee7362e34293bc9cd79fb90c
ldu13 <ldu13@myseneca.ca>
2022-02-18 17:35:08 -0500

    a3

--- blog-service.js
+++ blog-service.js
@@ -6,31 +6,28 @@ let categories = []
 
 module.exports.initialize = function() {
     return new Promise((resolve, reject) => {
-        fs.readFile('./data/posts.json', 'utf8', (err, data) => {
-            if(err) {
+        fs.readFile('./data/posts.json', 'utf8', (error, data) => {
+            if(error) {
                 reject("unable to read file")
             } else {
                 posts = JSON.parse(data)
-                //console.log(posts)
-                resolve(posts)
             }           
         })
 
-        fs.readFile('./data/categories.json', 'utf8', (err, data) => {
-            if(err) {
+        fs.readFile('./data/categories.json', 'utf8', (error, data) => {
+            if(error) {
                 reject("unable to read file")
             } else {
                 categories = JSON.parse(data)
-                //console.log(categories)
-                resolve(categories)
             }           
         })
+        resolve()
     })
 }
 
 module.exports.getAllPosts = function() {
     return new Promise((resolve, reject) => {
-        if(posts.length === 0) {
+        if(posts.length == 0) {
             reject("no results returned")
         } else {
             resolve(posts)
@@ -41,15 +38,14 @@ module.exports.getAllPosts = function() {
 module.exports.getPublishedPosts = function() {
     let publishedPosts = []
     return new Promise((resolve, reject) => {
-        if(posts.length === 0) {
+        for(let i = 0; i < posts.length; i++) {
+            if(posts[i].published == true) {
+                publishedPosts.push(posts[i])
+            }
+        }
+        if(publishedPosts.length == 0) {
             reject("no results returned")
         } else {
-            for(let i = 0; i < posts.length; i++) {
-                if(posts[i].published === true) {
-                    //publishedPosts += posts[i]  //only display[object object]
-                    publishedPosts.push(posts[i])
-                }
-            }
             resolve(publishedPosts)
         }              
     })                       
@@ -57,10 +53,70 @@ module.exports.getPublishedPosts = function() {
 
 module.exports.getCategories = function() {
     return new Promise((resolve, reject) => {
-        if(categories.length === 0){
+        if(categories.length == 0){
             reject("no result returned")
         }else{
             resolve(categories)
         }              
+    })
+}
+
+// p2-3 Adding an "addPost" function
+module.exports.addPost = function (postData) {
+    return new Promise((resolve, reject) => {
+        if (postData.published == undefined) {
+            postData.published = false;
+        } else {
+            postData.published = true;
+        }
+        postData.id = posts.length + 1
+        posts.push(postData)       
+        resolve()
+    })
+}
+
+// p4-1 Add the getPostsByCategory(category) Function  
+module.exports.getPostsByCategory = function(category) {
+    let selectedPosts = []
+    return new Promise((resolve, reject) => {   
+        for(let i = 0; i < posts.length; i++){
+            if(posts[i].category == category){
+                selectedPosts.push(posts[i]);
+            }
+        }   
+        if (selectedPosts.length == 0) {
+            reject("no results returned")
+        } else {
+            resolve(selectedPosts)
+        }
+    })
+}
+
+// p4-2 Add the getPostsByMinDate(minDateStr) Function  
+module.exports.getPostsByMinDate = function(minDateStr) {
+    let selectedPosts = []
+    return new Promise((resolve, reject) => {
+        posts.forEach(post => {
+            if (new Date(post.postDate) >= new Date(minDateStr)) {
+                selectedPosts.push(post)
+            }
+        })
+        if (selectedPosts.length == 0) {
+            reject("no results returned")
+        } else {
+            resolve(selectedPosts)
+        }
+    })
+}
+
+// p4-3 Add the getPostById(id) Function  
+module.exports.getPostById = function(newId) {
+    return new Promise((resolve, reject) => {
+        let selectedPosts = posts.filter(({id}) => id == newId);
+        if (selectedPosts.length == 0) {
+            reject("no results returned")
+        } else {
+            resolve(selectedPosts)
+        }
     })
 }
